f7d317911c4dfdcb08def393d3b62e2fcd8e253f
dlij <82123948+dlij@users.noreply.github.com>
2022-01-10 09:21:27 +1100

    Print

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+      int time = 10;
+      if (time <= 10) {
+        System.out.println("Good morning.");
+      } else if (time < 17) {
+        System.out.println("Good afternoon.");
+      } else if (time <= 24) {
+        System.out.println("Good Night");
+      } else {
+        System.out.println("Not a time lol.");
+      }
+    }
+  }
+  
