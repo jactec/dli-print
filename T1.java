221c0b01c4dd65b9ec4b66073823cd65d592146a
jactec <someone@somewhere.nowhere>
2022-01-10 09:56:56 +1100

    Fib

--- T1.java
+++ T1.java
@@ -1,5 +1,24 @@
 public class T1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        printFib();
+    }
+
+    private static void printFib() {
+
+        int count = 7, num1 = 0, num2 = 1;
+        System.out.print("Fibonacci Series of " + count + " numbers:");
+
+        for (int i = 1; i <= count; ++i) {
+            System.out.print(num1 + " ");
+
+            /*
+             * On each iteration, we are assigning second number
+             * to the first number and assigning the sum of last two
+             * numbers to the second number
+             */
+            int sumOfPrevTwo = num1 + num2;
+            num1 = num2;
+            num2 = sumOfPrevTwo;
+        }
     }
 }
