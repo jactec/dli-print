6f29aa4fe4cee5edc1d2dfc798849fba50b235b7
jactec <someone@somewhere.nowhere>
2022-01-10 09:29:16 +1100

    change to live time

--- Main.java
+++ Main.java
@@ -1,14 +1,16 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-      int time = 10;
+      Date now = new Date();
+      int time = now.getHours();
+
       if (time <= 10) {
         System.out.println("Good morning.");
       } else if (time < 17) {
         System.out.println("Good afternoon.");
-      } else if (time <= 24) {
-        System.out.println("Good Night");
       } else {
-        System.out.println("Not a time lol.");
+        System.out.println("Good Night");
       }
     }
   }
