72e48d92e5e73f617cbfa32d534deb0e5aae8d58
dlij <82123948+dlij@users.noreply.github.com>
2022-01-18 17:55:42 +1100

    Sum all numbers 1-100

---
+++ T2.java
@@ -0,0 +1,11 @@
+public class T2 {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
+//prints addition of all numbers 1-100
