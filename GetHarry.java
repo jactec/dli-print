3daa3c64153c613585daa476bfa6581d584ec402
jactec <someone@somewhere.nowhere>
2022-01-19 09:25:44 +1100

    Question 5

---
+++ GetHarry.java
@@ -0,0 +1,15 @@
+/**
+ * Question: read the file harry.txt, and print all the lines which contains word "Harry"
+ * 
+ * Hint: 
+ *  1. This is about read a file from hard disk, you might need find java apis/docs to work on it. (estimate: 3 days)
+ *  2. This is not a easy job, many new concepts were introduced. Get ready, be patiant. (estimate: 3 days)
+ *  3. Make it working (2 days)
+ *  4. If it working in 8 days, it is awsome.
+ * 
+ */
+
+
+public class GetHarry {
+    
+}
