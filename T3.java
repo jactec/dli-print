e69ff7a7b1da9022b0bc643782f1912c3caec811
dlij <82123948+dlij@users.noreply.github.com>
2022-01-18 18:16:14 +1100

    2n +1

---
+++ T3.java
@@ -0,0 +1,11 @@
+public class T3 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            int num = 2 * i + 1;
+            System.out.println(num);
+        }
+
+    }
+
+}
