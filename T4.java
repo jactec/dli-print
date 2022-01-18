54b16433f6a8bcefb29ffc905cce0a15affcdf16
jactec <someone@somewhere.nowhere>
2022-01-19 08:47:12 +1100

    dive in to understand what is num value

--- T4.java
+++ T4.java
@@ -3,6 +3,7 @@ public class T4 {
         int sum = 0;
         for (int i = 1; i <= 10; i++) {
             int num = 1/(i*(i+1));
+            System.out.println(num);
             sum = sum + num;
         }
         System.out.println(sum);
