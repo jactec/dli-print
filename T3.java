d5a14ecca2d51b4355a329900225bfc48e3fdf45
dlij <82123948+dlij@users.noreply.github.com>
2022-01-18 18:40:09 +1100

    adds them up

--- T3.java
+++ T3.java
@@ -1,11 +1,11 @@
 public class T3 {
     public static void main(String[] args) {
-int sum = 0;
+        int sum = 0;
         for (int i = 1; i <= 10; i++) {
             int num = 2 * i + 1;
             sum = sum + num;
         }
-System.out.println(sum);
+        System.out.println(sum);
     }
 
 }
