6912fb0541e0c82747b2331a2f3785b515821937
dlij <82123948+dlij@users.noreply.github.com>
2022-01-18 18:39:32 +1100

    adds them all together

---
+++ T4.java
@@ -0,0 +1,11 @@
+public class T4 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            int num = 1/(i*(i+1));
+            System.out.println(num);
+        }
+
+    }
+
+}
