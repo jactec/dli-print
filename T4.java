b9ca1daab16c2d2d5fd929f94df346f3c50b6fc4
dlij <82123948+dlij@users.noreply.github.com>
2022-01-19 08:43:47 +1100

    add

--- T4.java
+++ T4.java
@@ -1,11 +1,11 @@
 public class T4 {
     public static void main(String[] args) {
-
+        int sum = 0;
         for (int i = 1; i <= 10; i++) {
             int num = 1/(i*(i+1));
-            System.out.println(num);
+            sum = sum + num;
         }
-
+        System.out.println(sum);
     }
 
 }
