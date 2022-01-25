ba68dcb5a2c506f4e191c940d889e6230ea6bdfd
dlij <82123948+dlij@users.noreply.github.com>
2022-01-25 11:53:11 +1100

    Prints the results of the pattern

---
+++ Pattern.java
@@ -0,0 +1,11 @@
+public class Pattern {
+    public static void main(String[] args) {
+
+        for (int fn = 1; fn < 10; fn++) {
+            for (int sn = 1; sn < 10; sn++) {
+                int res = fn * sn;
+                System.out.println(res);
+            }
+        }
+    }
+}
