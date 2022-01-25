2bc7cf687a3f1feea240c94575ce8f12493c0b4f
dlij <82123948+dlij@users.noreply.github.com>
2022-01-25 14:05:50 +1100

    Prints out the whole operation

--- Pattern.java
+++ Pattern.java
@@ -4,7 +4,7 @@ public class Pattern {
         for (int fn = 1; fn < 10; fn++) {
             for (int sn = 1; sn < 10; sn++) {
                 int res = fn * sn;
-                System.out.println(res);
+                System.out.println(fn + "x" + sn + " = " + res);
             }
         }
     }
