d92a860a6df8b1360f464376f0e28bd000e57184
dlij <82123948+dlij@users.noreply.github.com>
2022-01-26 13:58:21 +1100

    solved t5

--- T4.java
+++ T4.java
@@ -3,11 +3,11 @@ public class T4 {
         float sum = 0;
         for (int i = 1; i <= 10; i++) {
             float v = i;
-            float num = 1/(v*(v+1));
+            float num = 1 / (v * (v + 1));
             sum = sum + num;
         }
         System.out.println(sum);
-        
+
     }
 
 }
--- T5.java
+++ T5.java
@@ -1,11 +1,14 @@
 
-
 // 题目：求1+2!+3!+…+20!的和
-
-
 
 public class T5 {
     public static void main(String[] args) {
-
+        for (int i = 1; i <= 20; i++) {
+            int itemValue = 1;
+            for (int j = i; j >= 1; j--) {
+                itemValue = itemValue * j;
+            }
+            System.out.println(itemValue);
+        }
     }
 }
