786ec0b8e557220d581dd2b711439fce71f228b8
dlij <82123948+dlij@users.noreply.github.com>
2022-01-28 09:37:14 +1100

    gert

--- T5.java
+++ T5.java
@@ -4,7 +4,7 @@
 public class T5 {
     public static void main(String[] args) {
         double sum = 0;
-        for (int index = 1; index <= 20; index++) {
+        for (double index = 1; index <= 20; index++) {
             double itemValue = 1;
             for (double number = index; number >= 1; number--) {
                 itemValue = itemValue * number;
